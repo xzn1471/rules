cf8c29967cfde660a0e04b4b3f0d23acfc4d0759
BOBOLAOSHIV587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2024-10-25 05:07:00 +0800

    Update idaily.js
    iDaily每日环球视野

--- Task/idaily.js
+++ Task/idaily.js
@@ -21,17 +21,17 @@
 【Surge】
 -----------------
 [Script]
-iDaily每日环球视野 = type=cron,cronexp=5 0 * * *,script-path=https://raw.githubusercontent.com/evilbutcher/QuantumultX/master/check_in/idaily/idaily.js
+iDaily每日环球视野 = type=cron,cronexp=5 0 * * *,script-path=https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/refs/heads/main/Task/idaily.js
 
 【Loon】
 -----------------
 [Script]
-cron "5 0 * * *" script-path=https://raw.githubusercontent.com/evilbutcher/QuantumultX/master/check_in/idaily/idaily.js, tag=iDaily每日环球视野
+cron "5 0 * * *" script-path=https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/refs/heads/main/Task/idaily.js, tag=iDaily每日环球视野
 
 【Quantumult X】
 -----------------
 [task_local]
-5 0 * * * https://raw.githubusercontent.com/evilbutcher/QuantumultX/master/check_in/idaily/idaily.js, tag=iDaily每日环球视野
+5 0 * * * https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/refs/heads/main/Task/idaily.js, tag=iDaily每日环球视野
 
 【Icon】
 透明：https://raw.githubusercontent.com/evilbutcher/QuantumultX/master/picture/idaily_tran.png
