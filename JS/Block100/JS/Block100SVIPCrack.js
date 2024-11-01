b44a570cd90a5777558c465ff175c27a2714f747
BOBOLAOSHIV587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2024-11-01 22:25:42 +0800

    Update Block100SVIPCrack.js

--- JS/Block100/JS/Block100SVIPCrack.js
+++ JS/Block100/JS/Block100SVIPCrack.js
@@ -1,17 +1,19 @@
 /*
  *
  *
-脚本功能：拦截100解锁超级会员
-软件版本：3.0.2【最高支持版本】
-下载地址：http://t.cn/A6MLFAJf
-脚本作者：Hausd0rff
-更新时间：2022-02-15
+# 个人备用
+# 脚本功能：拦截100解锁超级会员
+# 软件版本：3.0.2【最高支持版本】
+# 下载地址：http://t.cn/A6MLFAJf
+# 原脚本作者：Hausd0rff、ycq007
+# 脚本作者：BOBOLAOSHIV587
+# 更新时间：2022-02-15
 *******************************
 
 [rewrite_local]
 
 # > 拦截100解锁超级会员
-^https?:\/\/tagit\.hyhuo\.com\/cypt\/block100\/get_vip_info$ url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/Block100SVIPCrack.js
+^https?:\/\/tagit\.hyhuo\.com\/cypt\/block100\/get_vip_info$ url script-response-body https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/Block100/JS/Block100SVIPCrack.js
 
 [mitm] 
 
