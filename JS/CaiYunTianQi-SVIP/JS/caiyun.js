44ff24f78884678ff4b84350fdbc0585dd0adf3f
BOBOLAOSHIV587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2024-11-02 00:18:16 +0800

    Update caiyun.js

--- JS/CaiYunTianQi-SVIP/JS/caiyun.js
+++ JS/CaiYunTianQi-SVIP/JS/caiyun.js
@@ -8,7 +8,7 @@
 
 ===========QuantumultX===========
 [rewrite_local]
-^https:\/\/biz\.caiyunapp\.com\/v2\/user.+ url script-response-body https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/caiyun.js
+^https:\/\/biz\.caiyunapp\.com\/v2\/user.+ url script-response-body https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/CaiYunTianQi-SVIP/JS/caiyun.js
 
 
 [MITM]
