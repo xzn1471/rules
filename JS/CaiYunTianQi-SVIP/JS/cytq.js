d184f9459138635a50842f0777f5a665d34207ba
BOBOLAOSHIV587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2024-11-02 00:23:45 +0800

    Update cytq.js

--- JS/CaiYunTianQi-SVIP/JS/cytq.js
+++ JS/CaiYunTianQi-SVIP/JS/cytq.js
@@ -1,6 +1,7 @@
 /*
  *
  *
+个人备用
 脚本功能：彩云天气
 更新时间：2024年8.29
 使用声明：此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖！
@@ -11,11 +12,11 @@
 # 普通版广告
 ^https:\/\/ad\.cyapi\.cn\/v2\/req\?app_name=weather url reject-dict
 # 赏叶赏花
-^https:\/\/wrapper\.cyapi\.cn\/v1\/activity\?app_name=weather url script-response-body https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/cytq.js
+^https:\/\/wrapper\.cyapi\.cn\/v1\/activity\?app_name=weather url script-response-body https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/CaiYunTianQi-SVIP/JS/cytq.js
 # 解锁vip
-^https:\/\/biz\.cyapi\.cn\/v2\/user url script-response-body https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/cytq.js
+^https:\/\/biz\.cyapi\.cn\/v2\/user url script-response-body https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/CaiYunTianQi-SVIP/JS/cytq.js
 # 卫星云图 48小时预报
-^https:\/\/wrapper\.cyapi\.cn\/v1\/(satellite|nafp\/origin_images) url script-request-header https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/cytq.js
+^https:\/\/wrapper\.cyapi\.cn\/v1\/(satellite|nafp\/origin_images) url script-request-header https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/CaiYunTianQi-SVIP/JS/cytq.js
 
 [mitm]
 hostname = *.cyapi.cn
