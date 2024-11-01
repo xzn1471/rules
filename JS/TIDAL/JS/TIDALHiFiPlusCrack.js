f68dc6c6595913a0139094c0aaf7b58138d0c93b
BOBOLAOSHIV587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2024-11-01 22:12:45 +0800

    Update TIDALHiFiPlusCrack.js

--- JS/TIDAL/JS/TIDALHiFiPlusCrack.js
+++ JS/TIDAL/JS/TIDALHiFiPlusCrack.js
@@ -1,15 +1,16 @@
 /******************************
-
-脚本功能：TIDAL解锁HiFi Plus
-软件版本：2.48.0【美区下载】
-下载地址：http://t.cn/A662gqIO
-脚本作者：Hausd0rff
-更新时间：2022-08-05
+# 个人备用
+# 脚本功能：TIDAL解锁HiFi Plus
+# 软件版本：2.48.0【美区下载】
+# 下载地址：http://t.cn/A662gqIO
+# 原脚本作者：Hausd0rff
+# 脚本作者：BOBOLAOSHIV587
+# 更新时间：2022-08-05
 *******************************
 [rewrite_local]
 # > TIDAL解锁HiFi Plus
-^https?:\/\/api\.tidal\.com\/v1\/users\/\d+\/subscription.+ url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/TIDALHiFiPlusCrack.js
-^https?:\/\/api\.tidal\.com\/v1\/tracks/\d+\/playbackinfopostpaywall.+ url script-analyze-echo-response https://raw.githubusercontent.com/yqc007/QuantumultX/master/TidalHiFiPlusCrack.js
+^https?:\/\/api\.tidal\.com\/v1\/users\/\d+\/subscription.+ url script-response-body https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/TIDAL/JS/TIDALHiFiPlusCrack.js
+^https?:\/\/api\.tidal\.com\/v1\/tracks/\d+\/playbackinfopostpaywall.+ url script-analyze-echo-response https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/TIDAL/JS/TIDALHiFiPlusCrack.js
 
 [mitm] 
 hostname = api.tidal.com
