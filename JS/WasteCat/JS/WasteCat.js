095677982454162be03e49a5591999bb5bcdb414
BOBOLAOSHIV587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2024-11-02 16:13:16 +0800

    Update WasteCat.js

--- JS/WasteCat/JS/WasteCat.js
+++ JS/WasteCat/JS/WasteCat.js
@@ -1,22 +1,16 @@
 /******************************
-
-脚本功能：拦截猫+解锁订阅
-下载地址：https://is.gd/eiw1MI
-软件版本：1.3.26
-脚本作者：彭于晏💞
-更新时间：2024-10-11
-问题反馈：QQ+89996462
-QQ会员群：779392027💞
-TG反馈群：https://t.me/plus8889
-TG频道群：https://t.me/py996
-使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
-
+# 个人自用
+# 脚本功能：拦截猫+解锁订阅
+# 下载地址：https://is.gd/eiw1MI
+# 软件版本：1.3.26
+# 脚本作者：彭于晏💞
+# 更新时间：2024-10-11
 
 *******************************
 
 [rewrite_local]
 
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/WasteCat.js
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/WasteCat/JS/WasteCat.js
 
 [mitm] 
 
