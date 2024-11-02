6def2d56360cb5e3f1d871d0f092cd16ef11e76d
BOBOLAOSHIV587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2024-11-02 16:21:04 +0800

    Update xmind.js

--- JS/Xmind/JS/xmind.js
+++ JS/Xmind/JS/xmind.js
@@ -1,21 +1,16 @@
 /******************************
-
-脚本功能：XMind思维导图+解锁VIP
-下载地址：https://is.gd/Ozmrz0
-软件版本：23.06
-脚本作者：彭于晏💞
-更新时间：2023-7-14
-问题反馈：QQ+89996462
-QQ会员群：779392027💞
-TG反馈群：https://t.me/plus8889
-TG频道群：https://t.me/py996
-使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+# 个人自用
+# 脚本功能：XMind思维导图+解锁VIP
+# 下载地址：https://is.gd/Ozmrz0
+# 软件版本：23.06
+# 脚本作者：彭于晏💞
+# 更新时间：2023-7-14
 
 *******************************
 
 [rewrite_local]
 
-^http[s]?:\/\/www.xmind.cn\/_res\/devices url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/xmindv.js
+^http[s]?:\/\/www.xmind.cn\/_res\/devices url script-response-body https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/Xmind/JS/xmind.js
 
 [mitm] 
 
