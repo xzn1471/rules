71f6b84d185c10e0f18f4b0f7b55dcb609d686fd
BOBOLAOSHIV587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2024-11-02 16:07:07 +0800

    Update WallCraftFProCrack.js

--- JS/WallCraft/JS/WallCraftFProCrack.js
+++ JS/WallCraft/JS/WallCraftFProCrack.js
@@ -1,22 +1,20 @@
 /*
  *
  *
-脚本功能：WallCraft解锁专业版
-软件版本：2.8.3
-下载地址：http://t.cn/A6iO7Eht
-脚本作者：Hausd0rff
-更新时间：2022-02-06
-电报频道：https://t.me/yqc_123
-问题反馈：https://t.me/yqc_777
-使用声明：⚠️此脚本仅供学习与交流，
-        请勿转载与贩卖！⚠️⚠️⚠️
+# 个人自用
+# 脚本功能：WallCraft解锁专业版
+# 软件版本：2.8.3
+# 下载地址：http://t.cn/A6iO7Eht
+# 脚本作者：Hausd0rff
+# 更新时间：2022-02-06
+
 
 *******************************
 
 [rewrite_local]
 
 # > WallCraft解锁永久专业版
-^https?:\/\/billing-ios\.wallpaperscraft\.com\/verify_receipt\/remove_ads$ url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/WallCraftFProCrack.js
+^https?:\/\/billing-ios\.wallpaperscraft\.com\/verify_receipt\/remove_ads$ url script-response-body https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/WallCraft/JS/WallCraftFProCrack.js
 
 [mitm] 
 
