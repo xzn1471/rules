e7a3ae5b11cfb1ec2680e4c7a857c892ef6d6d10
BOBOLAOSHIV587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2024-11-02 12:02:30 +0800

    Update WPSDocerVIPuserCrack.js

--- JS/WPS/JS/WPSDocerVIPuserCrack.js
+++ JS/WPS/JS/WPSDocerVIPuserCrack.js
@@ -1,19 +1,17 @@
 /******************************
 
-脚本功能：WPS解锁稻壳会员
-软件版本：11.26.0
-下载地址：http://t.cn/A6ZD2hl3
-脚本作者：Hausd0rff
-更新时间：2022-07-05
-脚本发布：https://t.me/yqc_123
-问题反馈：https://t.me/yqc_777
-使用声明：⚠️此脚本仅供学习与交流，
-        请勿转载与贩卖！⚠️⚠️⚠️
+# 个人备用
+# 脚本功能：WPS解锁稻壳会员
+# 软件版本：11.26.0
+# 下载地址：http://t.cn/A6ZD2hl3
+# 脚本作者：Hausd0rff
+# 更新时间：2022-07-05
+
 *******************************
 [rewrite_local]
 # > WPS解锁稻壳会员
-^https?:\/\/account\.wps\.cn\/api\/v3\/mine\/vips url script-response-body https://raw.githubusercontent.com/yqc007/QuantumultX/master/WPSDocerVIPuserCrack.js
-^https?:\/\/.+\.(docer.)?wps.cn\/(user\/v1\/vip|android\/mb\/buy|download\/v1\/ios|partner\/invoke\/usable|(api|rank)\/v1(\/mobile\/mb)?\/detail) url script-request-header https://raw.githubusercontent.com/yqc007/QuantumultX/master/WPSDocerVIPowerCrack.js
+^https?:\/\/account\.wps\.cn\/api\/v3\/mine\/vips url script-response-body https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/WPS/JS/WPSDocerVIPuserCrack.js
+^https?:\/\/.+\.(docer.)?wps.cn\/(user\/v1\/vip|android\/mb\/buy|download\/v1\/ios|partner\/invoke\/usable|(api|rank)\/v1(\/mobile\/mb)?\/detail) url script-request-header https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/WPS/JS/WPSDocerVIPowerCrack.js
 
 [mitm] 
 hostname = *.docer.wps.cn, vipapi.wps.cn, account.wps.cn
