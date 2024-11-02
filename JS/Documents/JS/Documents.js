363abbbb0a0ae14812b8080ff527d3de31d2a20a
BOBOLAOSHIV587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2024-11-02 15:01:34 +0800

    Update Documents.js

--- JS/Documents/JS/Documents.js
+++ JS/Documents/JS/Documents.js
@@ -1,21 +1,16 @@
 /******************************
-
-脚本功能：documents文件管理器+解锁订阅
-下载地址：https://is.gd/DLjoIn
-软件版本：v8.5
-脚本作者：彭于晏💞
-更新时间：2023-8-20
-问题反馈：QQ+89996462
-QQ会员群：779392027💞
-TG反馈群：https://t.me/plus8889
-TG频道群：https://t.me/py
-使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+# 个人自用
+# 脚本功能：documents文件管理器+解锁订阅
+# 下载地址：https://is.gd/DLjoIn
+# 软件版本：v8.5
+# 脚本作者：彭于晏💞
+# 更新时间：2023-8-20
 
 *******************************
 
 [rewrite_local]
 
-^https:\/\/license\.pdfexpert\.com\/api\/2\.0\/documents\/subscription\/refresh url script-request-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/documents.js
+^https:\/\/license\.pdfexpert\.com\/api\/2\.0\/documents\/subscription\/refresh url script-request-body https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/Documents/JS/Documents.js
 
 [mitm] 
 
