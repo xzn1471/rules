121a1f8ebc8140ba6828786936fdab341429e925
BOBOLAOSHIV587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2024-11-02 15:54:18 +0800

    Update ServerBee.js

--- JS/ServerBee/JS/ServerBee.js
+++ JS/ServerBee/JS/ServerBee.js
@@ -1,27 +1,19 @@
 /******************************
-
-脚本功能：ServerBee——解锁订阅
-下载地址：https://is.gd/NyslNJ
-软件版本：1.3.1
-脚本作者：彭于晏💞
-更新时间：2024-5-9
-问题反馈：QQ+89996462
-QQ会员群：779392027💞
-TG反馈群：https://t.me/plus8889
-TG频道群：https://t.me/py996
-使用声明：此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+# 个人自用
+# 脚本功能：ServerBee——解锁订阅
+# 下载地址：https://is.gd/NyslNJ
+# 软件版本：1.3.1
+# 脚本作者：彭于晏💞
+# 更新时间：2024-5-9
 
 *******************************
 
 
 [rewrite_local]
-
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/ServerBee.js
-
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/ServerBee.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/ServerBee/JS/ServerBee.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/ServerBee/JS/ServerBee.js
 
 [mitm] 
-
 hostname = api.revenuecat.com
 
 *******************************/
