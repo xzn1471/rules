65b1d0db422eedc4a0b73435825e21cf4ae4686d
BOBOLAOSHIV587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2024-11-06 23:34:36 +0800

    Update Flightradar24.js

--- JS/Flightradar24/JS/Flightradar24.js
+++ JS/Flightradar24/JS/Flightradar24.js
@@ -7,7 +7,7 @@
 >「 注意事项 」         使用此脚本，会导致AppleStore无法切换账户，解决方法[关闭QX切换账户，或关闭MITM，或删除脚本，或去设置媒体与购买项目处切换ID]
 
 [rewrite_local]
-^https?:\/\/mobile\.flightradar24\.com\/mobile\/(user-session|subscribe) url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/Flightradar24.js
+^https?:\/\/mobile\.flightradar24\.com\/mobile\/(user-session|subscribe) url script-response-body https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/Flightradar24/JS/Flightradar24.js
 
 [mitm]
 hostname = mobile.flightradar24.com
