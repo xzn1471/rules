d9473dda9dff654336f3940c93c64113534a31d1
BOBOLAOSHIV587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2024-11-07 01:11:28 +0800

    Update Craft.js

--- JS/Craft/JS/Craft.js
+++ JS/Craft/JS/Craft.js
@@ -4,13 +4,11 @@
 >「 脚本作者 」         M̆̈̆̈ĭ̈̆̈k̆̈̆̈ĕ̈
 >「 电报频道 」         https://t.me/TrollStoreKios 
 >「 更新时间 」         2024-10-05
->「 注意事项 」         如需引用请注明出处，谢谢合作！
 >「 注意事项 」         使用此脚本，会导致AppleStore无法切换账户，解决方法[关闭QX切换账户，或关闭MITM，或删除脚本，或去设置媒体与购买项目处切换ID]
->「 额外说明 」         请勿传播或售卖此脚本
 
 
 [rewrite_local]
-^https?:\/\/(api|docs)\.craft\.do\/(api\/)?(auth\/v\d\/profile|subscription\/(receipt|teams\/get-subscriptions))  url script-response-body https://raw.githubusercontent.com/Mike-offers/Rewrite/refs/heads/master/QuantumultX/Craft.js
+^https?:\/\/(api|docs)\.craft\.do\/(api\/)?(auth\/v\d\/profile|subscription\/(receipt|teams\/get-subscriptions))  url script-response-body https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/Craft/JS/Craft.js
 
 [mitm]
 hostname = api.craft.do , docs.craft.do
