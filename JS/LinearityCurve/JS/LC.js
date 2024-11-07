65ca2789042e1aee0069743dca1c8acf0f5cf954
BOBOLAOSHIV587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2024-11-07 23:59:22 +0800

    Update LC.js

--- JS/LinearityCurve/JS/LC.js
+++ JS/LinearityCurve/JS/LC.js
@@ -8,8 +8,8 @@
 ********************************
 [rewrite_local]
 # >Linearity Curve插画和图形绘-解锁订阅
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/.+$) url script-response-body https://raw.githubusercontent.com/afengye/QX/main/lc.js
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/.+$) url script-request-header https://raw.githubusercontent.com/afengye/QX/main/lc.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/.+$) url script-response-body https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/LinearityCurve/JS/LC.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/.+$) url script-request-header https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/LinearityCurve/JS/LC.js
 [mitm] 
 hostname = api.revenuecat.com
 *******************************/
