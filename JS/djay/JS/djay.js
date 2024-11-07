0eee663c5ba6d510306ccbd74e1f14d3dfaf1e38
BOBOLAOSHIV587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2024-11-08 00:06:07 +0800

    Create djay.js

---
+++ JS/djay/JS/djay.js
@@ -0,0 +1,35 @@
+/*******************************
+脚本功能：djayDJ应用-解锁订阅
+# https://apps.apple.com/app/id450527929
+脚本作者：afengye
+注意事项：查看频道说明
+频道地址：https://t.me/afengye
+使用声明：️仅供学习交流, 🈲️商业用途
+********************************
+[rewrite_local]
+# >djayDJ应用-解锁订阅
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/.+$) url script-response-body https://raw.githubusercontent.com/afengye/QX/main/djay.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/.+$) url script-request-header https://raw.githubusercontent.com/afengye/QX/main/djay.js
+[mitm] 
+hostname = api.revenuecat.com
+*******************************/
+let obj = {};
+
+if(typeof $response == "undefined") {
+  delete $request.headers["x-revenuecat-etag"];
+  delete $request.headers["X-RevenueCat-ETag"];
+  obj.headers = $request.headers;
+}else {
+  let body = JSON.parse(typeof $response != "undefined" && $response.body || null);
+  if(body && body.subscriber) {
+    const product_id = "com.algoriddim.djay_iphone_free.pro.yearly";
+    const entitlement = "pro";
+    let data = {"expires_date": "2999-01-01T00:00:00Z","original_purchase_date":"2021-01-01T00:00:00Z","purchase_date": "2021-01-01T00:00:00Z","ownership_type": "PURCHASED","store": "app_store"};
+    let subscriber = body.subscriber;
+    subscriber.entitlements[(entitlement)] = subscriber.subscriptions[(product_id)] = data;        
+    subscriber.entitlements[(entitlement)].product_identifier = product_id;   
+    obj.body = JSON.stringify(body);
+  } 
+}
+
+$done(obj);
