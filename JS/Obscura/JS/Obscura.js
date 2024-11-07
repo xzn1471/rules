512c1a9bcece3f28beae20db4aa62f297431be14
BOBOLAOSHIV587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2024-11-07 23:48:49 +0800

    Update Obscura.js

--- JS/Obscura/JS/Obscura.js
+++ JS/Obscura/JS/Obscura.js
@@ -1,13 +1,15 @@
 /*******************************
 脚本功能：Obscura专业相机-解锁订阅
+# https://apps.apple.com/app/id1579306989
 脚本作者：afengye
 注意事项：查看频道说明
 频道地址：https://t.me/afengye
 使用声明：️仅供学习交流, 🈲️商业用途
 ********************************
 [rewrite_local]
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/.+$) url script-response-body https://raw.githubusercontent.com/afengye/QX/main/obscura.js
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/.+$) url script-request-header https://raw.githubusercontent.com/afengye/QX/main/obscura.js
+# >Obscura专业相机-解锁订阅
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/.+$) url script-response-body https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/Obscura/JS/Obscura.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/.+$) url script-request-header https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/Obscura/JS/Obscura.js
 [mitm] 
 hostname = api.revenuecat.com
 *******************************/
