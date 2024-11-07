a587076bed830859a875ba33fcc72263fe3dbd6e
BOBOLAOSHIV587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2024-11-07 23:43:45 +0800

    Update SparkMaill.js

--- JS/SparkMaill/JS/SparkMaill.js
+++ JS/SparkMaill/JS/SparkMaill.js
@@ -6,8 +6,8 @@
 使用声明：️仅供学习交流, 🈲️商业用途
 ********************************
 [rewrite_local]
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/.+$) url script-response-body https://raw.githubusercontent.com/afengye/QX/main/spark.js
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/.+$) url script-request-header https://raw.githubusercontent.com/afengye/QX/main/spark.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/.+$) url script-response-body https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/SparkMaill/JS/SparkMaill.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/.+$) url script-request-header https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/SparkMaill/JS/SparkMaill.js
 [mitm] 
 hostname = api.revenuecat.com
 *******************************/
