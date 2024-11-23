2822d45847e61fa1b0444a2b83cd37d0349c52a2
波波老师V587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2024-11-24 05:42:45 +0800

    Yosum Premium订阅

--- JS/Yosum/JS/Yosum.js
+++ JS/Yosum/JS/Yosum.js
@@ -16,7 +16,7 @@
 # XiaoMao · TG通知频道：https://t.me/xiaomaoJT
 # XiaoMao · Tg脚本频道：https://t.me/XiaoMaoScript
 # XiaoMao · GitHub仓库：https://github.com/xiaomaoJT/QxScript
-
+#https://apps.apple.com/cn/app/yosum-%E8%AE%A2%E9%98%85%E7%AE%A1%E7%90%86/id6451242119?uo=4
 
 使用方法：
 1、QX > 右下角风车 > 重写 > 规则资源 > 引用以下脚本 > 打开资源解析器
