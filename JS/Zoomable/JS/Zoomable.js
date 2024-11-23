ea94eab2f4a66ece4f8da6d84b6afdb330d6a18e
波波老师V587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2024-11-24 05:44:13 +0800

    Zoomable解锁Pro

--- JS/Zoomable/JS/Zoomable.js
+++ JS/Zoomable/JS/Zoomable.js
@@ -16,7 +16,7 @@
 # XiaoMao · TG通知频道：https://t.me/xiaomaoJT
 # XiaoMao · Tg脚本频道：https://t.me/XiaoMaoScript
 # XiaoMao · GitHub仓库：https://github.com/xiaomaoJT/QxScript
-
+#https://apps.apple.com/cn/app/zoomable-%E6%A1%8C%E9%9D%A2%E6%B5%8F%E8%A7%88%E5%99%A8/id1568442831?uo=4
 
 使用方法：
 1、QX > 右下角风车 > 重写 > 规则资源 > 引用以下脚本 > 打开资源解析器
