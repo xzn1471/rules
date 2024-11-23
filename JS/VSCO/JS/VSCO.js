55686fbe284c171f738b7870909cbd9df7e81919
波波老师V587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2024-11-24 06:06:30 +0800

    VSCO Pro长期会员订阅

--- JS/VSCO/JS/VSCO.js
+++ JS/VSCO/JS/VSCO.js
@@ -16,7 +16,7 @@
 # XiaoMao · TG通知频道：https://t.me/xiaomaoJT
 # XiaoMao · Tg脚本频道：https://t.me/XiaoMaoScript
 # XiaoMao · GitHub仓库：https://github.com/xiaomaoJT/QxScript
-
+#https://apps.apple.com/cn/app/vsco-photo-video-editor/id588013838?uo=4
 
 使用方法：
 1、QX > 右下角风车 > 重写 > 规则资源 > 引用以下脚本 > 打开资源解析器
