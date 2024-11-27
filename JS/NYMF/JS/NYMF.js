1a98da8f1d1545210f443be339c230df465fb756
波波老师V587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2024-11-27 08:12:58 +0800

    NYMF Pro图片 视频 故事 模特资源解锁 去除马赛克

---
+++ JS/NYMF/JS/NYMF.js
@@ -0,0 +1,33 @@
+/**************************
+ *  * @Author: XiaoMao
+ * @LastMod: 2023-06-25
+ *
+
+
+\N\Y\M\F\ \P\r\o\图\片\ \视\频\ \故\事\ \模\特\资\源\解\锁\ \去\除\马\赛\克\
+
+********************************
+# 小版本更新请查看更新日志 ｜ 或加入xiaomao组织⬇️
+# 微信公众号 【小帽集团】
+# XiaoMao · TG通知频道：https://t.me/xiaomaoJT
+# XiaoMao · Tg脚本频道：https://t.me/XiaoMaoScript
+# XiaoMao · GitHub仓库：https://github.com/xiaomaoJT/QxScript
+
+
+使用方法：
+1、应用下载地址：https://apps.apple.com/us/app/nymf-art-nude-female-beauty/id1495951015?uo=4
+
+2、QX > 右下角风车 > 重写 > 规则资源 > 引用以下脚本 > 打开资源解析器
+https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoNYMF.js
+
+********************************
+
+[mitm]
+hostname = nymfapp.com
+
+[rewrite_local]
+https:\/\/nymfapp\.com\/api\/(photos|posts|stories|compose) url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/nymf.js
+
+ ***************/
+
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('3 0=$t.0;3 1=6.9(0);a 5=$b.c;4(/^d:\\/\\/e\\.f\\/g\\/(h|s|i|j)?/.k(5)){4(1.n){1.o((2)=>{2.p=7;2.q="r";2.8=7})}}0=6.m(1);$l({0});',30,30,'body|obj|el|var|if|requestUrl|JSON|false|censored|parse|let|request|url|https|nymfapp|com|api|photos|stories|compose|test|done|stringify|length|forEach|pro|price_category|available|posts|response'.split('|'),0,{}))
