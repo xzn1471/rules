64beb6b585e8908a3f421ab65b9bce0c493469a9
波波老师V587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2024-12-08 01:04:16 +0800

    Xmind-思维导图

--- JS/Xmind/JS/Xmind.js
+++ JS/Xmind/JS/Xmind.js
@@ -1,15 +1,40 @@
-/*
-# 个人自用
-# 脚本功能：解锁Xmind思维导图订阅到2030年
-# 脚本作者：Stranger
-# 电报频道：https://t.me/qixinscience
-# 更新时间：2022.08.13
+/*************************************
+
+项目名称：Xmind-思维导图
+下载地址：https://t.cn/AipCL5zE
+脚本作者：chxm1023
+电报频道：https://t.me/chxm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
+使用说明：先登录Xmind账号在恢复购买
+
+**************************************
 
 [rewrite_local]
-https?:\/\/www\.xmind\.(cn|net)\/\_res\/devices url script-response-body https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/Xmind/JS/Xmind.js
+^https?:\/\/(?:www\.)?xmind\..*\/.+\/(devices|token\/.+) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Xmind.js
 
 [mitm]
-hostname = www.xmind.cn, www.xmind.net
-*/
+hostname = *xmind.*
 
-var _0x49754b=JSON['parse']($response['body']);_0x49754b['license']['status']='sub';_0x49754b['license']['expireTime']=0x1b8d90e4000;$done({'body':JSON['stringify'](_0x49754b)});
+*************************************/
+
+
+var chxm1023 = JSON.parse($response.body);
+const vip = /https:\/\/www\.xmind\.cn\/_res\/devices/;
+const token = /https:\/\/xmind\.cn\/_res\/token\/.+/;
+
+if (vip.test($request.url)) {
+    chxm1023 = {
+      "license" : {
+        "status" : "sub",
+        "expireTime" : 4092599349000
+      },
+      "_code" : 200,
+      "raw_data" : "GfxYgAqnrQ\/ggD9UwqnZyAj6FKnopXzM8s5m3eZLOsmVr4FwCzv41qTmgi\/u72cv+jpaAoljaEPti1twzOS7X7KUPY1KNJ2xalbS7SXbvFHSvs21QXjaUtIOkeJWAl4\/vHl4IvMeHTBVqD8mFCXOmvnBPLRMAJEPgHEJYF1InvQ="};
+}
+
+if (token.test($request.url)) {
+    chxm1023.expireDate = 4092599349;
+    chxm1023.token = "f50633ea8eb04cbb85962b99c47045d7AjOobEGo";
+}
+
+$done({body : JSON.stringify(chxm1023)});
