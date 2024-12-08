88657ea05ff538b9fef9ba5ec37399a97266a428
波波老师V587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2024-12-08 19:15:19 +0800

    Noto 笔记 - 功能强大的笔记应用,解锁永久订阅 🔓

--- JS/Noto/JS/Noto.js
+++ JS/Noto/JS/Noto.js
@@ -3,7 +3,7 @@ var obj = JSON.parse(body);
 
 obj.subscriber.entitlements = {
       "pro": {
-        "expires_date": "2029-06-14T13:54:33Z",
+        "expires_date": "2999-06-14T13:54:33Z",
         "grace_period_expires_date": null,
         "product_identifier": "com.lkzhao.editor.pro.ios.monthly",
         "purchase_date": "2022-06-11T13:54:33Z"
@@ -13,7 +13,7 @@ obj.subscriber.entitlements = {
 obj.subscriber.subscriptions ={
       "com.lkzhao.editor.pro.ios.monthly": {
         "billing_issues_detected_at": null,
-        "expires_date": "2029-06-14T13:54:33Z",
+        "expires_date": "2999-06-14T13:54:33Z",
         "grace_period_expires_date": null,
         "is_sandbox": false,
         "original_purchase_date": "2022-06-11T13:54:34Z",
