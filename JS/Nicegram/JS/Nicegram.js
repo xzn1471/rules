c339f10ff4c09c4008c498394a16373bb1cd491d
波波老师V587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2024-12-23 03:03:48 +0800

    Nicegram会员解锁

--- JS/Nicegram/JS/Nicegram.js
+++ JS/Nicegram/JS/Nicegram.js
@@ -1,18 +1,20 @@
 /*
-脚本功能：解锁Nicegram会员
-脚本作者：R·E
-支持版本：商店最新 1.0.7
-更新时间：2022.09.23
-问题反馈：https://t.me/Functional_Store_Hub
-使用声明：⚠️⚠️⚠️此脚本仅供学习与交流，禁止转载与贩卖！⚠️⚠️⚠️
-
+项目名称： nicegram
+项目作者： David
+下载地址： https://apps.apple.com/us/app/nicegram-ai-chat-for-telegram/id1608870673?l=en-GB
+使用说明： 要激活高级版，请单击底部的“助手”，然后单击“释放您的 Nicegram 福利”，最后单击底部的“开始”。完成此操作后，Premium 应该已解锁
+使用声明： 仅供个人参考学习交流，勿用于其它用途
 
 [rewrite_local]
-^https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\w+$ url script-echo-response https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/Nicegram/JS/Nicegram.js
-
+^https?:\/\/nicegram\.cloud\/api\/v\d\/(ai-assistant\/purchase-list|user\/info|telegram\/auth) url script-response-body https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/Nicegram/JS/nicegram.js
 [mitm]
-hostname = restore-access.indream.app
-
+hostname = nicegram.cloud
 */
+var body = $response.body;
 
-const a=typeof $task!='undefined';const b=typeof $httpClient!='undefined';if(b){console['log']('\x0aR·E\x20Nicegram\x20Script\x20Log:\x0aNicegram\x20Premium已解锁😎');$done({'response':{'status':0xc8,'body':'{\x22data\x22:\x20{\x22premiumAccess\x22:\x20true}}'}});}else if(a){console['log']('\x0aR·E\x20Nicegram\x20Script\x20Log:\x0aNicegram\x20Premium已解锁😎');$done({'status':'HTTP/1.1\x20200\x20OK','headers':{'Content-Type':'application/json'},'body':'{\x22data\x22:\x20{\x22premiumAccess\x22:\x20true}}'});}else{console['log']('\x0aR·E\x20Nicegram\x20Script\x20Log:\x0aNicegram\x20Premium已解锁😎');$done({'status':0xc8,'body':'{\x22data\x22:\x20{\x22premiumAccess\x22:\x20true}}'});}
+body = body.replace(/"store_subscription":false/g, '"store_subscription":true');
+body = body.replace(/"lifetime_subscription":false/g,'"lifetime_subscription":true');
+body = body.replace(/"subscription":false/g,'"subscription":true');
+
+
+$done({ body });
