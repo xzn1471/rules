d91ce79e9448c4ee3059c234ba8b918323565bf0
波波老师V587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2025-01-11 15:28:15 +0800

    PornHub Unlock Premium

--- JS/PornHub/JS/PornHubPremiumCrack.js
+++ JS/PornHub/JS/PornHubPremiumCrack.js
@@ -1,16 +1,21 @@
 /******************************
-# 个人备用
-# 脚本功能：PornHub Unlock Premium
-# 网页版本：2.0.1
-# 网站地址：https://shrtm.nu/PornHub
-# 脚本作者：Hausd0rff、ycq007
-# 更新时间：2022-03-09
+
+脚本功能：PornHub Unlock Premium
+网页版本：2.0.1
+网站地址：https://shrtm.nu/PornHub
+脚本作者：Hausd0rff
+更新时间：2022-03-09
+电报频道：https://t.me/yqc_123
+问题反馈：https://t.me/yqc_777
+使用声明：⚠️此脚本仅供学习与交流，
+        请勿转载与贩卖！⚠️⚠️⚠️
 *******************************
 [rewrite_local]
 # > PornHub Unlock Premium By Hausd0rff
-^https?:\/\/cn\.pornhubpremium\.com\/ url script-request-header https://raw.githubusercontent.com/BOBOLAOSHIV587/Rules/main/JS/PornHub/JS/PornHubPremiumCrack.js
+^https?:\/\/cn\.pornhubpremium\.com\/ url script-request-header https://raw.githubusercontent.com/yqc007/QuantumultX/master/PornHubPremiumCrack.js
+^https?:\/\/cn\.pornhubpremium\.com\/ url script-response-body https://raw.githubusercontent.com/Peng-Lx/lxpscript/master/JS/Hub.js
 
-[mitm] 
+[mitm]
 hostname = cn.pornhubpremium.com
 
 *******************************/
