c2420e3dbaa6202480a40a2053e46229a30cb232
波波老师V587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2025-01-22 00:28:01 +0800

    Revenuecat系列解锁合集

--- JS/Revenuecat/JS/Reheji.js
+++ JS/Revenuecat/JS/Reheji.js
@@ -1,10 +1,10 @@
 /*************************************
 
-# 项目名称：Revenuecat系列解锁合集
-# 更新日期：2025-01-15
-# 脚本作者：@ddm1023
-# 电报频道：https://t.me/ddm1023
-# 使用声明：⚠️仅供参考，🈲转载与售卖！
+项目名称：Revenuecat系列解锁合集
+更新日期：2025-01-19
+脚本作者：@ddm1023
+电报频道：https://t.me/ddm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
 # 旧：
 # [rewrite_local]
 # >Revenuecat 系列解锁合集
@@ -54,6 +54,7 @@ const bundle = {
 };
 
 const listua = {
+  'Accountit': { name: 'spenditPlus', id: 'DesignTech.SIA.Spendit.Plus.Lifetime', cm: 'sjb' },  //Accountit-日常记账
   'Phtoto%20Swiper': { name: 'pro', id: 'rc_499_life', cm: 'sjb' },  //PhotoDeleteSwipe- 照片清理
   'ShellBean': { name: 'pro', id: 'com.ningle.shellbean.iap.forever', cm: 'sjb' },  //ShellBean-SSH终端
   'Wishy': { name: 'Wishy Subscription', id: 'wishy_lifetime_subscription', cm: 'sjc' },  //Wishy-记录愿望
