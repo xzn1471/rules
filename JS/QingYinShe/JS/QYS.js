c8a8acbdf60f401e736c9e0dae79c618d92b457d
波波老师V587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2025-03-19 21:33:24 +0800

    轻音社vip

---
+++ JS/QingYinShe/JS/QYS.js
@@ -0,0 +1,25 @@
+/******************************
+
+脚本功能：轻音社
+软件版本：1.1.4
+下载地址：https://apps.apple.com/us/app/%E8%BD%BB%E9%9F%B3%E7%A4%BE/id1584504085
+更新时间：2024年3月10日 
+电报频道：https://t.me/GieGie777
+使用声明：此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖！
+*******************************
+
+[rewrite_local]
+# > 轻音社vip
+^https?:\/\/fuciyuanbang.ciyuans.com\/fuciyuanphp\/community\/social url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/qys.js
+
+[mitm]
+hostname = fuciyuanbang.ciyuans.com
+
+*******************************/
+
+
+var body = $response.body; 
+let obj = JSON.parse($response.body);
+obj.result.vip = "1" ;
+obj.result.name = "https://t.me/GieGie777" ;
+$done({body: JSON.stringify(obj)});
