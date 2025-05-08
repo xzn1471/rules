be2947c47bb607d077a79b942568de6199e407a2
波波老师V587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2025-05-08 23:42:46 +0800

    Nicegram解锁Premium

--- JS/Nicegram/JS/JS-3/NicegramProCrack.js
+++ JS/Nicegram/JS/JS-3/NicegramProCrack.js
@@ -10,13 +10,11 @@
 > 问题反馈：ddgksf2013@163.com
 > 特别提醒：如需转载请注明出处，谢谢合作！
 > 解锁步骤：https://t.me/ddgksf2021/5439
-> 特别说明：⚠️⚠️⚠️
-          本脚本仅供学习交流使用，禁止转载售卖
-          ⚠️⚠️⚠️
+> 特别说明：⚠️⚠️本脚本仅供学习交流使用，禁止转载售卖
 
+***********************************
 
 [rewrite_local]
-  
 # > Nicegram☆解锁会员权限（2024-02-24）@ddgksf2013
 ^https?:\/\/nicegram\.cloud\/api\/v\d\/(ai-assistant\/purchase-list|user\/info|telegram\/auth) url script-response-body https://github.com/ddgksf2013/MoYu/raw/master/NicegramProCrack.js
 
