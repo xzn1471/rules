38de667c8cb8fb657b169f60cf43417bb0b9e716
波波老师V587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2025-05-09 05:14:52 +0800

    MissAV网页去广告

--- JS/MissAV/JS/MissAV.js
+++ JS/MissAV/JS/MissAV.js
@@ -8,7 +8,6 @@
  * Github：https://github.com/limbopro/Adblock4limbo
  
 ******************************
-
 [rewrite_local]
 ^https?:\/\/missav\.(ws|com|ai)\/(?!(.*(api|login|cdn-cgi|verify|auth|captch|(\.(js|css|jpg|jpeg|png|webp|gif|zip|woff|woff2|m3u8|mp4|mov|m4v|avi|mkv|flv|rmvb|wmv|rm|asf|asx|mp3|json|ico|otf|ttf))))).* url script-response-body https://limbopro.com/Adguard/Adblock4limbo.js
 
